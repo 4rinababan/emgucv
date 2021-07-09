52af46b830647e1057a0171f0d0fb97c38feb3fb
ARI NABABAN <arinababan123@gmail.com>
2021-07-09 17:17:12 +0700

    2131

---
+++ open_cv/open_cv/Class1.vb
@@ -0,0 +1,185 @@
+﻿Imports Emgu.CV.Structure
+Imports Emgu.CV
+Public Class Class1
+    Private _eiginImages As Image(Of Gray, Single)()
+    Private _avgImage As Image(Of Gray, Single)
+    Private _eigenValues As Matrix(Of Single)()
+    Private _labels As String()
+    Private _eigenDistanceThreeshold As Double
+    Private _eigenDistance1 As Single
+    Private hasil As String
+
+    Public Property EigenImages As Image(Of Gray, Single)()
+        Get
+            Return _eiginImages
+
+        End Get
+        Set(ByVal value As Image(Of Gray, Single)())
+            _eiginImages = value
+        End Set
+    End Property
+
+    Public Property Labels() As String()
+        Get
+            Return _labels
+        End Get
+        Set(ByVal value As String())
+            _labels = value
+        End Set
+    End Property
+
+    Public Property EigenDistanceThreeshold As Double
+        Set(ByVal value As Double)
+            _eigenDistanceThreeshold = value
+        End Set
+        Get
+            Return _eigenDistanceThreeshold
+        End Get
+    End Property
+
+    Public Property AverangeImages As Image(Of Gray, Single)
+        Get
+            Return _avgImage
+        End Get
+        Set(ByVal value As Image(Of Gray, Single))
+            _avgImage = value
+        End Set
+    End Property
+
+    Public Property EigenValues() As Matrix(Of Single)()
+        Get
+            Return _eigenValues
+        End Get
+        Set(ByVal value As Matrix(Of Single)())
+            _eigenValues = value
+        End Set
+    End Property
+
+    Private Sub New()
+
+    End Sub
+
+    Public Sub New(images As Image(Of Gray, [Byte])(), ByRef termCrit As MCvTermCriteria)
+        Me.New(images, GenerateLabels(images.Length), termCrit)
+    End Sub
+
+    Private Shared Function GenerateLabels(ByVal size As Integer) As [String]()
+        Dim labels As [String]() = New String(size - 1) {}
+        For i As Integer = 0 To size - 1
+            labels(i) = i.ToString()
+        Next
+        Return labels
+    End Function
+
+    Public Sub New(images As Image(Of Gray, [Byte])(), ByVal labels As [String](), ByRef termCrit As MCvTermCriteria)
+        Me.New(images, labels, 0, termCrit)
+    End Sub
+
+    Public Sub New(ByVal images As Image(Of Gray, [Byte])(), ByVal labels As [String](), ByVal eigenDistanceThreshold As Double, ByRef termCrit As MCvTermCriteria)
+        Debug.Assert(images.Length = labels.Length, "Jumlah gambar harus sama dengan jumlah label")
+        Debug.Assert(eigenDistanceThreshold >= 0.0, "Eigen Threesold harus selalu >= 0.0")
+
+        CalcEigenObjects(images, termCrit, _eiginImages, _avgImage)
+
+        _eigenValues = Array.ConvertAll(Of Image(Of Gray, [Byte]), Matrix(Of Single)) _
+            (images, Function(img As Image(Of Gray, [Byte])) New Matrix(Of Single)(EigenDecomposite(img, _eiginImages, _avgImage)))
+        _labels = labels
+        _eigenDistanceThreeshold = eigenDistanceThreshold
+    End Sub
+
+    Public Shared Sub CalcEigenObjects(ByVal trainingImages As Image(Of Gray, [Byte])(),
+            ByRef termCrit As MCvTermCriteria, ByRef eigenImages As Image(Of Gray, [Single])(),
+                                       ByRef avg As Image(Of Gray, [Single]))
+        Dim width As Integer = trainingImages(0).Width
+        Dim height As Integer = trainingImages(0).Height
+
+        Dim inObjs As IntPtr() = Array.ConvertAll(Of Image(Of Gray, [Byte]), IntPtr)(trainingImages,
+Function(img As Image(Of Gray, [Byte])) img.Ptr)
+
+        If termCrit.max_iter <= 0 OrElse termCrit.max_iter > trainingImages.Length Then
+            termCrit.max_iter = trainingImages.Length
+        End If
+
+        Dim maxEigenObjs As Integer = termCrit.max_iter
+
+        eigenImages = New Image(Of Gray, Single)(maxEigenObjs - 1) {}
+        For i As Integer = 0 To eigenImages.Length - 1
+            eigenImages(i) = New Image(Of Gray, Single)(width, height)
+        Next
+
+        Dim eigObjs As IntPtr() = Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr) _
+            (eigenImages, Function(img As Image(Of Gray, [Single])) img.Ptr)
+
+        avg = New Image(Of Gray, [Single])(width, height)
+
+        CvInvoke.cvCalcEigenObjects(inObjs, termCrit, eigObjs, Nothing, avg.Ptr)
+    End Sub
+
+    Public Shared Function EigenDecomposite(ByVal src As Image(Of Gray, [Byte]),
+        ByVal eigenImages As Image(Of Gray, [Single])(),
+        ByVal avg As Image(Of Gray, [Single])) As Single()
+        Return CvInvoke.cvEigenDecomposite(src.Ptr, Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr) _
+            (eigenImages, Function(img As Image(Of Gray, [Single])) img.Ptr), avg.Ptr)
+    End Function
+
+    Public Function EigenProjection(ByVal eigenValue As Single()) As Image(Of Gray, [Byte])
+        Dim res As Image(Of Gray, [Byte]) = New Image(Of Gray, Byte)(_avgImage.Width, _avgImage.Height)
+
+        CvInvoke.cvEigenProjection(Array.ConvertAll(Of Image(Of Gray, [Single]), IntPtr) _
+            (_eiginImages, Function(img As Image(Of Gray, [Single])) img.Ptr), eigenValue, _avgImage.Ptr, res.Ptr)
+        Return res
+    End Function
+
+    Public Function GetEigenDistances(ByVal image As Image(Of Gray, [Byte])) As Single()
+        Using eigenvalue As New Matrix(Of Single)(EigenDecomposite(image, _eiginImages, _avgImage))
+            Return Array.ConvertAll(Of Matrix(Of Single), Single) _
+                (_eigenValues, Function(eigenValueI As Matrix(Of Single)) _
+                                   CSng(CvInvoke.cvNorm(eigenvalue.Ptr, eigenValueI.Ptr, Emgu.CV.CvEnum.NORM_TYPE.CV_L2, IntPtr.Zero)))
+
+
+        End Using
+    End Function
+
+    Public ReadOnly Property Get_Eigen_Distance() As Single
+        Get
+            Return _eigenDistance1
+        End Get
+    End Property
+
+    Public Sub FindMostSimilarObject(ByVal image As Image(Of Gray, [Byte]), ByRef index As Integer,
+                                     ByRef eigenDistance As Single, ByRef label As [String])
+
+        Dim dist As Single() = GetEigenDistances(image)
+        index = 0
+        eigenDistance = dist(0)
+        For i As Integer = 1 To dist.Length - 1
+            If dist(i) < eigenDistance Then
+                index = i
+                eigenDistance = dist(i)
+            End If
+        Next
+        label = Labels(index)
+
+    End Sub
+
+    Public Function Recognize(ByVal image As Image(Of Gray, [Byte]), ByVal status As Integer) As [String]
+        Dim index As Integer
+        Dim eigenDistance As Single
+        Dim label As [String]
+
+        FindMostSimilarObject(image, index, eigenDistance, label)
+        _eigenDistance1 = eigenDistance
+
+        If status = 1 Then
+            hasil = If((_eigenDistanceThreeshold <= 0 OrElse eigenDistance > _eigenDistanceThreeshold _
+                AndAlso eigenDistance < 5000), _labels(index), "Tidak Dikenal")
+        ElseIf status = 0 Then
+            hasil = If((_eigenDistanceThreeshold <= 0 OrElse eigenDistance > _eigenDistanceThreeshold _
+                AndAlso eigenDistance < 4700), _labels(index), "Tidak Dikenal")
+
+        End If
+        Return (hasil)
+
+    End Function
+
+End Class
---
+++ open_cv/open_cv/Form1.vb
@@ -0,0 +1,181 @@
+﻿Imports System.IO
+Imports System.Xml
+Imports System.Threading
+
+Imports Emgu.CV
+Imports Emgu.Util
+Imports Emgu.CV.Structure
+Imports Emgu.CV.CvEnum
+Imports DirectShowLib
+
+Public Class Form1
+    Private currentFrame As Image(Of Bgr, [Byte])
+    Private grabber As Capture
+    Private face As HaarCascade
+
+    Private font As New MCvFont(CvEnum.FONT.CV_FONT_HERSHEY_COMPLEX, 0.3, 0.3)
+    Private result As Image(Of Gray, Byte) = Nothing
+    Private gray As Image(Of Gray, Byte) = Nothing
+    Private trainingImages As New List(Of Image(Of Gray, Byte))()
+    Private labels As New List(Of String)()
+    Private NamePersons As New List(Of String)()
+    Private Contrain As Integer, Numlabels As Integer, g As Integer
+    Private name As String, names As String = Nothing
+    Private Wtd As Integer
+    Private DefaultThreshold As Double = 3000
+    Private Names_List As New List(Of String)()
+    Private Names_List_ID As New List(Of Integer)()
+
+    Dim Pengenalanwajah As Class1
+
+    Public Sub New()
+
+
+        ' This call is required by the designer.
+        InitializeComponent()
+        bacakamera()
+        face = New HaarCascade("haarcasade_fontalface_alt.xml")
+        loadtrainingdata()
+        ComboBox1.Text = Convert.ToString(ComboBox1.Items(0))
+
+        TextBox1.Text = Convert.ToString(GetThreshold)
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+    Public Sub loadtrainingdata()
+        If File.Exists(Application.StartupPath + "/db_img/TrainedLabels.xml") Then
+            Try
+                Names_List.Clear()
+                Names_List_ID.Clear()
+                trainingImages.Clear()
+                Numlabels = 0
+                labels.Clear()
+
+                Dim filestream As FileStream = File.OpenRead(Application.StartupPath + "/db_img/TrainedLabels.xml")
+                Dim filelength As Long = filestream.Length
+                Dim xmlBytes As Byte() = New Byte(filelength - 1) {}
+                filestream.Read(xmlBytes, 0, CInt(filelength))
+                filestream.Close()
+
+                Dim xmlStream As New MemoryStream(xmlBytes)
+
+                Using xmlreader As XmlReader = XmlTextReader.Create(xmlStream)
+                    While xmlreader.Read()
+                        If xmlreader.IsStartElement() Then
+                            Select Case xmlreader.Name
+                                Case "NAMA"
+                                    If xmlreader.Read() Then
+                                        Names_List_ID.Add(Names_List.Count)
+                                        '0, 1, 2, 3.....
+                                        Names_List.Add(xmlreader.Value.Trim())
+                                        labels.Add(xmlreader.Value.Trim())
+                                        Numlabels += 1
+                                    End If
+                                    Exit Select
+                                Case "NamaFile"
+                                    If xmlreader.Read() Then
+                                        trainingImages.Add(New Image(Of Gray, Byte)(Application.StartupPath + "\db_img" + xmlreader.Value.Trim()))
+
+                                    End If
+                                    Exit Select
+                            End Select
+                        End If
+                    End While
+                End Using
+                Contrain = Numlabels
+            Catch ex As Exception
+                MessageBox.Show("Database Masih Kosong", "Training Face", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            End Try
+        End If
+    End Sub
+    Public ReadOnly Property GetThreshold() As Double
+        Get
+            Return DefaultThreshold
+        End Get
+    End Property
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "Start" Then
+            Try
+                grabber = New Capture(ComboBox1.SelectedIndex)
+                SetThreshold(Convert.ToDouble(TextBox1.Text))
+                grabber.QueryFrame()
+
+                AddHandler Application.Idle, AddressOf FrameGrabber
+
+                Button1.Text = "Stop"
+                ComboBox1.Enabled = False
+                TextBox1.Enabled = False
+                Dim termCrit As New MCvTermCriteria(Contrain, 0.001)
+                Pengenalanwajah = New Class1(trainingImages.ToArray(), labels.ToArray(), GetThreshold, termCrit)
+            Catch ex As Exception
+                MessageBox.Show("Gagal Mendapatkan Device Camera", "Failed")
+            End Try
+
+        ElseIf Button1.Text = "Stop" Then
+            RemoveHandler Application.Idle, AddressOf FrameGrabber
+
+            If grabber IsNot Nothing Then
+                grabber.Dispose()
+                PictureBox1.Image = Nothing
+                Button1.Text = "Start"
+                ComboBox1.Enabled = True
+                TextBox1.Enabled = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim FrmTraining As New Form2
+        FrmTraining.Show()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Public Function SetThreshold(ByVal nilai As Double)
+        DefaultThreshold = nilai
+        Return DefaultThreshold
+    End Function
+
+    Private Sub FrameGrabber(ByVal sender As Object, ByVal e As EventArgs)
+        NamePersons.Add("")
+        currentFrame = grabber.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+
+        gray = currentFrame.Convert(Of Gray, [Byte])()
+        Dim faceDetected As MCvAvgComp()() = gray.DetectHaarCascade(face, 1.2, 8, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, New Size(50, 50))
+
+        For Each f As MCvAvgComp In faceDetected(0)
+            result = currentFrame.Copy(f.rect).Convert(Of Gray, Byte)().Resize(100, 100, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC)
+
+            result._EqualizeHist()
+            currentFrame.Draw(f.rect, New Bgr(Color.BlueViolet), 2)
+            If trainingImages.ToArray().Length <> 0 Then
+                name = Pengenalanwajah.Recognize(result, 0)
+                Dim match_value As Integer = CInt(Pengenalanwajah.Get_Eigen_Distance)
+                Dim NamaDetected As [String] = "Nama : " + name.ToUpper()
+                Dim EigDist As [String] = "Distance : " + Convert.ToString(match_value)
+
+                currentFrame.Draw(NamaDetected, font, New Point(f.rect.X - 2, f.rect.Y - 13), New Bgr(Color.BurlyWood))
+                currentFrame.Draw(EigDist, font, New Point(f.rect.X - 2, f.rect.Y - 4), New Bgr(Color.BurlyWood))
+
+            End If
+
+            If Contrain < 1 Then
+                currentFrame.Draw("NAMA : Tidak Dikenal", font, New Point(f.rect.X - 2, f.rect.Y - 4), New Bgr(Color.BurlyWood))
+            End If
+        Next
+        PictureBox1.Image = currentFrame.ToBitmap
+    End Sub
+
+    Private Sub bacakamera()
+        Dim drivCam() As DsDevice
+        drivCam = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice)
+        For Each d As DsDevice In drivCam
+            ComboBox1.Items.Add(d.Name)
+        Next
+    End Sub
+
+End Class
